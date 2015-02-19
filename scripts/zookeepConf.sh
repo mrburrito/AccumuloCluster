2d53b20739c08ef2151e874723e013f89562fdfc
Gordon Shankman <gordon.shankman@altamiracorp.com>
2015-02-19 12:09:46 -0500

    Fixed init scripts; added missing comment and license links to initCluster.sh and fixing /cloud/data/zookeeper directory in zookeeperConf.sh. It was previously creating a " directory in /vagrant because the sed script added "/cloud/data/zookeeper" to the config file.

--- scripts/zookeepConf.sh
+++ scripts/zookeepConf.sh
@@ -3,6 +3,6 @@
 #<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">DubsAccumulo Vagrant Cluster</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/wjfrelo/vagrantAccumuloCluster" property="cc:attributionName" rel="cc:attributionURL">Wyatt J. Frelot</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/wjfrelo/vagrantAccumuloCluster" rel="dct:source">https://github.com/wjfrelo/vagrantAccumuloCluster</a>.
 
 mv /cloud/zookeeper/conf/zoo_sample.cfg /cloud/zookeeper/conf/zoo.cfg
-sed -i 's/\/tmp\/zookeeper/"\/cloud\/data\/zookeeper"/' /cloud/zookeeper/conf/zoo.cfg
+sed -i 's/\/tmp\/zookeeper/\/cloud\/data\/zookeeper/' /cloud/zookeeper/conf/zoo.cfg
 sed -i 's/#maxClientCnxns=60/maxClientCnxns=100/' /cloud/zookeeper/conf/zoo.cfg
 #/cloud/zookeeper/bin/./zkServer.sh start
